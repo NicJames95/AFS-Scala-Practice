56c778d3c0eec45b60d66a575b36c6d963b72d49
NicJames95 <nicjames1995@gmail.com>
2022-05-03 07:27:06 -0500

    Command Line Demo Commit

--- src/main/scala/com/udemy/scala/programming_in_depth/fileHandlingDemo/DemoACommandLine.scala
+++ src/main/scala/com/udemy/scala/programming_in_depth/fileHandlingDemo/DemoACommandLine.scala
@@ -1,5 +1,15 @@
 package com.udemy.scala.programming_in_depth.fileHandlingDemo
-
+// Read from Comand Line scala.io._
 object DemoACommandLine extends App{
+  println("Enter your name")
+  val name = scala.io.StdIn.readLine()
+  println(s"Hello $name")
 
+  println(s"Hello $name, Enter phone number")
+  val number = scala.io.StdIn.readLine()
+  println(s"Phone Number is $number")
+
+  println(s"Hello $name, Enter an age")
+  val age = scala.io.StdIn.readLine()
+  println(s"Age is $age")
 }
